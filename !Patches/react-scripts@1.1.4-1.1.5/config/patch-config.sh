5eaa9b5d2fd333231d300927805e288d46c3abb3
lilliputten <lilliputten@yandex.ru>
2018-11-29 06:54:13 +0300

    Patch dev-server to serve raw/fake files

--- !Patches/react-scripts@1.1.4-1.1.5/config/patch-config.sh
+++ !Patches/react-scripts@1.1.4-1.1.5/config/patch-config.sh
@@ -1,7 +1,8 @@
 #!/sbin/sh
 # @overview Patch tool (patch-tool) config file
+# @description Allow dev-server to serve raw/fake files, add appSrc into default module paths
 # @since 2018.02.25, 21:59
-# @version 2018.02.25, 21:59
+# @version 2018.11.29, 06:51
 
 # Root folder
 ROOTCD="node_modules"
